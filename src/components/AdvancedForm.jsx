04cb73d0f00c8f6f2af53452dc0c2c28164967d3
yashwant <yash.mangate1@gmail.com>
2023-09-14 17:21:39 +0530

    UPDATED: implemented Formik components (Formik, Field, ErrorMessage etc...) and learnt about nested objects and theirs validation using Yup

--- src/components/AdvancedForm.jsx
+++ src/components/AdvancedForm.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import * as Yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Link } from "react-router-dom";
+import FormFieldError from "./FormFieldError";
 
 const AdvancedForm = () => {
   const initialValues = {
     name: "",
     email: "",
     channel: "",
+    comments: "",
+    address: "",
+    social: {
+      facebook: "",
+      twitter: "",
+    },
   };
 
   const onSubmit = (values) => {
@@ -20,6 +27,12 @@ const AdvancedForm = () => {
       .email("Not a valid email")
       .required("Email is required"),
     channel: Yup.string().required("Channel name is required"),
+    comments: Yup.string().required("Comments is required"),
+    address: Yup.string().required("Address is required"),
+    social: Yup.object().shape({
+      facebook: Yup.string().required("Facebook is required"),
+      twitter: Yup.string().required("Twitter is required"),
+    }),
   });
 
   return (
@@ -45,7 +58,7 @@ const AdvancedForm = () => {
                 className="input-field"
               />
             </div>
-            <ErrorMessage name="name" />
+            <ErrorMessage name="name" component={FormFieldError} />
           </div>
           <div>
             <div className="input-group">
@@ -59,7 +72,9 @@ const AdvancedForm = () => {
                 className="input-field"
               />
             </div>
-            <ErrorMessage name="email" />
+            <ErrorMessage name="email">
+              {(msg) => <FormFieldError>{msg}</FormFieldError>}
+            </ErrorMessage>
           </div>
           <div>
             <div className="input-group">
@@ -73,7 +88,101 @@ const AdvancedForm = () => {
                 className="input-field"
               />
             </div>
-            <ErrorMessage name="channel" />
+            <ErrorMessage
+              name="channel"
+              render={(msg) => <div className="input-error">{msg}</div>}
+            />
+          </div>
+          <div>
+            <div className="input-group">
+              <label htmlFor="comments" className="input-label">
+                Comments
+              </label>
+              <Field
+                as="textarea"
+                id="comments"
+                name="comments"
+                className="input-field"
+              />
+            </div>
+            <ErrorMessage name="comments" component={FormFieldError} />
+          </div>
+
+          {/* address field using prop rendering */}
+          {/* <div>
+            <div className="input-group">
+              <label htmlFor="address" className="input-label">
+                Address
+              </label>
+              <Field name="address">
+                {(props) => {
+                  const { field, form, meta } = props;
+                  //   console.log(meta);
+                  return (
+                    <>
+                      <input
+                        type="text"
+                        id="address"
+                        className="input-field"
+                        name="address"
+                        {...field}
+                      />
+                      {meta.touched && meta.error ? (
+                        <p className="input-error">{meta.error}</p>
+                      ) : null}
+                    </>
+                  );
+                }}
+              </Field>
+            </div>
+          </div> */}
+
+          <div>
+            <div className="input-group">
+              <label htmlFor="address" className="input-label">
+                Address
+              </label>
+              <Field
+                type="text"
+                className="input-field"
+                name="address"
+                id="address"
+              />
+            </div>
+            <ErrorMessage name="address" component={FormFieldError} />
+          </div>
+
+          <div>
+            <div className="input-group">
+              <label htmlFor="facebook" className="input-label">
+                Facebook profile
+              </label>
+              <Field
+                type="text"
+                id="facebook"
+                name="social.facebook"
+                className="input-field"
+              />
+            </div>
+            <ErrorMessage name="social.facebook">
+              {(msg) => <FormFieldError>{msg}</FormFieldError>}
+            </ErrorMessage>
+          </div>
+
+          <div>
+            <div className="input-group">
+              <label htmlFor="twitter" className="input-label">
+                Twitter profile
+              </label>
+              <Field
+                type="text"
+                id="twitter"
+                name="social.twitter"
+                className="input-field"
+              />
+            </div>
+
+            <ErrorMessage name="social.twitter" component={FormFieldError} />
           </div>
 
           <button type="submit" className="button">
---
+++ src/components/FormFieldError.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FormFieldError = (props) => {
+  return <div className="input-error">{props.children}</div>;
+};
+
+export default FormFieldError;
